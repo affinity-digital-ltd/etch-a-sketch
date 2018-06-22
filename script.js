6d6053232ceb9dc5201d5378322928fad2507b16
Kris Quigley <kris@krisquigley.co.uk>
2018-06-22 15:55:49 +0100

    Made grid dynamic

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let container = document.querySelector('.container');
 
-const gridSize = 16;
+const gridSize = 32;
 const totalGridSize = gridSize * gridSize;
 
 for(let i = 1; i <= totalGridSize; i++) {
-  if (i % 16 === 1) {
+  if (i % gridSize === 1) {
     container.innerHTML += "<div class='clear'></div>";
   } else {
     container.innerHTML += '<div></div>';
