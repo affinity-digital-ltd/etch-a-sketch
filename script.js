f5ac2a6a1ccf7be6684586099d909801973ce631
Jonathon Day <jonathonday@Jonathons-MacBook-Air.local>
2018-07-19 10:39:41 +0100

    grid completed

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 let container = document.querySelector('.container');
 
-const gridSize = 16;
+const gridSize = 30;
 const totalGridSize = gridSize * gridSize;
 
-for(let i = 0; i < totalGridSize; i++) {
-  container.innerHTML += '<div></div>';
-  if (i % 16 === 1) {
-    container.innerHTML += '<div class='clear'></div>';
+for(let i = 1; i <= totalGridSize; i++) {
+  if (i % gridSize === 1) {
+    container.innerHTML += "<div class='clear'></div>";
+  } else {
+    container.innerHTML += '<div></div>';
   }
 }
-
-
-
