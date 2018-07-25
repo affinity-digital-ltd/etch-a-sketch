9b4d4300f7d3c0e1ea030a3a1080f8c3b4fe4e8c
Jonathon Day <jonathonday@Jonathons-MacBook-Air.local>
2018-07-25 18:34:19 +0100

    first pull request revisions

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 
-const resetButton = document.querySelector('button[data-behaviour="reset"]');
-const sizeButton = document.querySelector('button[data-behaviour="size"]');
-const rainbowButton = document.querySelector('button[data-behaviour="rainbow"]');
-const blackButton = document.querySelector('button[data-behaviour="black"]');
-const gameContainer = document.querySelector('.gameContainer');
+
+const resetButton = document.querySelector('button[data-behavior="reset"]');
+const sizeButton = document.querySelector('button[data-behavior="size"]');
+const rainbowButton = document.querySelector('button[data-behavior="rainbow"]');
+const blackButton = document.querySelector('button[data-behavior="black"]');
+const gameContainer = document.querySelector('div[data-behavior="container"]');
+
 let isRainbowPen = false
+
 sizeButton.addEventListener('click', () => {
   makeGrid()
 });
@@ -17,33 +20,31 @@ blackButton.addEventListener('click', () => {
   isRainbowPen = false
 });
 
-
+window.onload = function() {
+  grid(16);
+}
 
 //Create Grid
 function grid(size) {
   for(let i = 1; i <= (size * size); i++) {
     let gridCell = document.createElement('div');
-    gridCell.classList.add('grid');
+    gridCell.classList.add('gridDivs');
     gameContainer.appendChild(gridCell);
-    document.querySelectorAll('grid');
     gridCell.addEventListener('mouseover', () => colorGridCell(gridCell));
-    gridCell.addEventListener('click', () => gridCell.classList.remove('paint'));
   }
 }
-grid(16);
 
 //Resets the game and prompts the user for a numerical input for the size of the new grid.
-function makeGrid() {
-  let gridSize = prompt('How many squares would you like on each axis of the new grid?', 16, )
-  document.body.style.setProperty('--gridSize', gridSize)
-  let elementRemove = document.getElementsByClassName('grid')
-  while (elementRemove[0]) {
-    elementRemove[0].parentNode.removeChild(elementRemove[0])
-  }
+function makeGrid(gridSize) {
+  gridSize = prompt('How many squares would you like on each axis of the new grid?', 16, )
+  document.body.style.setProperty('--gridSize', gridSize);
+  gameContainer.innerHTML = '';
   grid(gridSize);
 }
 
-//Generate colours for pointer - default
+
+
+//Generate colours for pointer - default is black
 function penColor() {
   let penColor = 'black'
   if (isRainbowPen) {
@@ -59,10 +60,13 @@ function colorGridCell(gridCell) {
   gridCell.style.setProperty('background-color', penColor());
 }
 
+//Reset game to 16 * 16 grid to start again.
 resetButton.onclick = reloadPage;
 function reloadPage () {
   window.location.reload();
 }
+
+
 
 
   
